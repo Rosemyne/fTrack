b5bb6af4a49d5b2c5bd2b0fb65963f429f7d1584
Rosemyne <nyaeko@protonmail.com>
2024-04-23 00:37:24 -0600

    Added two classes for processing accounts and transactions

---
+++ accList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fTrack
+{
+
+    // Internal class for processing and "storing" accounts during run 
+    internal class accList
+    {
+
+    }
+}
---
+++ transList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fTrack
+{
+    // Internal class for processing transactions
+    internal class transList
+    {
+    }
+}
